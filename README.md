33c793a0d88a2ab0697195cb0b7e6cf2fd4f5879
Phil Tsarik <phil.tsarik@gmail.com>
2018-06-10 21:41:47 +0200

    ci: Add deploy script

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # My CV
 
+[![Travis](https://img.shields.io/travis/phts/my-cv.svg)](https://travis-ci.org/phts/my-cv)
+
 Written in LaTeX.
-Available in [PDF](https://github.com/phts/my-cv/raw/master/pdf/Phil%20Tsarik%20-%20CV.pdf) and [Markdown](https://github.com/phts/my-cv/blob/master/md/README.md).
+Available in [PDF](https://phts.github.io/my-cv/Phil%20Tsarik%20-%20CV.pdf) and [Markdown](https://phts.github.io/my-cv/README.md).
 
 Markdown is compiled using [pandoc](https://github.com/jgm/pandoc):
 
--- make.sh
+++ make.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+DIST=./dist
+
+mkdir -p "$DIST"
 
 # Convert to Markdown
 command -v pandoc >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-    mkdir -p ./md
-    pandoc -o md/README.md --to=markdown_github my-cv.tex
+  pandoc -o "$DIST/README.md" --to=markdown_github my-cv.tex
 else
-    echo "'pandoc' is not installed"
+  echo "'pandoc' is not installed"
 fi
 
 # Convert to PDF
 command -v pdflatex >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-    pdflatex my-cv.tex
-    mkdir -p ./pdf
-    mv -f my-cv.pdf pdf/Phil\ Tsarik\ -\ CV.pdf
+  pdflatex my-cv.tex
+  mv -f my-cv.pdf "$DIST/Phil Tsarik - CV.pdf"
 else
-    echo "'pdflatex' is not installed"
+  echo "'pdflatex' is not installed"
 fi
